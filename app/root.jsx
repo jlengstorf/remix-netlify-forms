c9a250bd3bc4b1748fed8b81ea89e634f230cab7
Jason Lengstorf <jason@lengstorf.com>
2022-02-17 21:29:33 -0800

    trying a static form

--- app/root.jsx
+++ app/root.jsx
@@ -25,39 +25,6 @@ export default function App() {
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
-
-        <style>
-          {`
-            .visually-hidden {
-              clip: rect(0 0 0 0);
-              clip-path: inset(50%);
-              height: 1px;
-              overflow: hidden;
-              position: absolute;
-              white-space: nowrap;
-              width: 1px;
-            }
-          `}
-        </style>
-        <form
-          method="post"
-          name="Test Form"
-          data-netlify="true"
-          action="/thanks/"
-          className="visually-hidden"
-        >
-          <label>
-            Name
-            <input name="name" type="text" />
-          </label>
-
-          <label>
-            Email
-            <input name="email" type="email" />
-          </label>
-
-          <button type="submit">Submit</button>
-        </form>
       </body>
     </html>
   );
