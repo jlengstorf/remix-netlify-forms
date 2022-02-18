4aeb1a8d95ba93877e383f5ef0a48572154547d5
Jason Lengstorf <jason@lengstorf.com>
2022-02-17 21:35:04 -0800

    trying again

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,4 +1,23 @@
 export default function Index() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const data = new FormData(form);
+
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': event.target.getAttribute('name'),
+        name: data.get('name'),
+        email: data.get('email'),
+      }),
+    })
+      .then(() => navigate('/thanks/'))
+      .catch((error) => alert(error));
+  };
+
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <h1>Form Test</h1>
